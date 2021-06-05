312fd02c22bf854ccf132adb509ece83ed42d13a
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-05 10:51:17 -0300

    implementing the attack method

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -9,16 +9,35 @@ new Vue({
         hasResult(){
             return this.playerLife == 0 || this.monsterLife == 0
         }
-
     },
     methods: {
         startGame() {
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+        }, 
+
+        attack(special) {
+            //testes
+            this.hurt('monsterLife',5,10, true)//ferir o monstro
+            this.hurt('playerLife',7,12, false)//ferir o jogador
+            //console.log(special, this.getRandom(5,10))
+        },
+
+        hurt(atr, min, max, special){
+          const plus = special ? 5 : 0
+          const hurt = this.getRandom(min + plus, max + plus)
+          this[atr] = Math.max(this.playerLife - hurt, 0)  //math.max para evitar numero negativo
+        },
+
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
         }
     },
     watch: {
-
+        hasResult(value) {
+            if(value) this.running = false
+        }
     }
 })
