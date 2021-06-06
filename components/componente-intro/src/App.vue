7ccc02bd470cbab8191d2273fb6a09d1cb981d3c
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-06 12:52:11 -0300

    adding project component-intro

---
+++ components/componente-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <h2>Contadores</h2>
+        {{ contador }}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            contador: 0
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
