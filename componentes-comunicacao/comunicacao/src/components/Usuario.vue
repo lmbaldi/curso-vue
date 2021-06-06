567d0f624fcee0cc5a27fe8c278bd0ffa2364616
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-06 20:49:50 -0300

    direct communication with props #01

--- componentes-comunicacao/comunicacao/src/components/Usuario.vue
+++ componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -5,7 +5,7 @@
         <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info />
+            <app-usuario-info :nome="nome" />
             <app-usuario-editar />
         </div>
     </div>
@@ -20,12 +20,12 @@ export default {
 
     data(){
         return {
-            nome: 'Pedro'
+            nome: 'Caio'
         }
     },
     methods: {
         alterarNome(){
-            this.nome = 'Ana'
+            this.nome = 'Bianca'
         }
     }
 }
