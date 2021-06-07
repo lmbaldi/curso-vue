3f8ef13b5d3e6872a93dd5418ec1f9cb8c450a96
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-06 22:03:15 -0300

    indirect communication using callback

--- componentes-comunicacao/comunicacao/src/components/Usuario.vue
+++ componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -1,53 +1,64 @@
 <template>
-    <div class="container">
-        <h1>Componente Usuário</h1>
-        <p>Esse é um componente muito legal!</p>
-        <p> Nome é <strong> {{ nome }} </strong></p>
-        <button @click="alterarNome">Alterar Nome</button>
-        <hr>
-        <div class="componentes">
-            <app-usuario-info :nome="nome" @nomeMudou="nome = $event.novo" />
-            <app-usuario-editar />
-        </div>
+  <div class="container">
+    <h1>Componente Usuário</h1>
+    <p>Esse é um componente muito legal!</p>
+    <p>
+      Nome é <strong> {{ nome }} </strong>
+    </p>
+    <button @click="alterarNome">Alterar Nome</button>
+    <hr />
+    <div class="componentes">
+      <app-usuario-info
+        :nome="nome"
+        @nomeMudou="nome = $event.novo"
+        :reiniciarFn="reiniciarNome"
+      />
+
+      <app-usuario-editar />
     </div>
+  </div>
 </template>
 
 <script>
-import AppUsuarioInfo from './UsuarioInfo'
-import AppUsuarioEditar from './UsuarioEditar'
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar },
+  components: { AppUsuarioInfo, AppUsuarioEditar },
 
-    data(){
-        return {
-            nome: 'Caio'
-        }
+  data() {
+    return {
+      nome: "Caio",
+    };
+  },
+  methods: {
+    alterarNome() {
+      this.nome = "Bianca"
     },
-    methods: {
-        alterarNome(){
-            this.nome = 'Bianca'
-        }
+
+    reiniciarNome() {
+        this.nome = "Caio"
     }
-}
+  },
+};
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .componentes {
-        display: flex;
-    }
+.componentes {
+  display: flex;
+}
 
-    .componentes > * {
-        margin: 10px;
-    }
+.componentes > * {
+  margin: 10px;
+}
 </style>
--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -1,46 +1,51 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-        <p> Nome do Usuario: <strong> {{ inverterNome() }} </strong></p>
-        <button @click="reiniciarNome"> Reiniciar Nome </button>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do Usuario: <strong> {{ inverterNome() }} </strong>
+    </p>
+    <button @click="reiniciarNome">Reiniciar Nome</button>
+    <button @click="reiniciarFn()">Reiniciar Nome (Callback)</button>
+
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        nome: {
-            type: String,
-            // required: true,
-            default: 'Anonimo'
-            //default: function() {
-            //    return Array(10).fill(0).join(', ');
-            //}
-        }
+  props: {
+    nome: {
+      type: String,
+      // required: true,
+      default: "Anonimo",
+      //default: function() {
+      //    return Array(10).fill(0).join(', ');
+      //}
+    },
+    reiniciarFn: Function
+  },
+
+  methods: {
+    inverterNome() {
+      return this.nome.split("").reverse().join("");
     },
 
-    methods: {
-        inverterNome(){
-            return this.nome.split('').reverse().join('')
-        },
-
-        reiniciarNome(){
-            const antigo = this.nome
-            this.nome = 'Caio'
-            this.$emit('nomeMudou', {
-                novo: this.nome,
-                antigo
-            })
-        }
-    }
-}
+    reiniciarNome() {
+      const antigo = this.nome;
+      this.nome = "Caio";
+      this.$emit("nomeMudou", {
+        novo: this.nome,
+        antigo,
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
