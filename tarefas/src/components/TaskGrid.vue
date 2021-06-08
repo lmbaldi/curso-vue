f4048037556eb2d74f11ae5aae4136e6ff6e6122
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-08 10:11:30 -0300

    task list component #01

---
+++ tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tasks">
+        <div v-for="task in tasks" :key="task.name">{{ task.name }}></div>    
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: { type: Array, required: true}
+    }
+}
+</script>
+
+
+<style>
+
+</style>
