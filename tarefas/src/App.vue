8bb6ead9a0790bc74a4efc8492a4fdd7f4da485f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-09 09:43:25 -0300

    new task component #01

--- tarefas/src/App.vue
+++ tarefas/src/App.vue
@@ -1,46 +1,62 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-		<TaskGrid :tasks="tasks"/>
-	</div>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-import TaskGrid from './components/TaskGrid.vue'
+import NewTask from "./components/NewTask.vue";
+import TaskGrid from "./components/TaskGrid.vue";
 
 export default {
-	components: { TaskGrid },
-	data(){
-		return {
-			tasks: [
-				{name: 'Lavar a louça', pending: false},
-				{name: 'Comprar blusa', pending: true},
-			]
-		}
-	}
-
-}
+  components: { NewTask, TaskGrid },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar a louça", pending: false },
+        { name: "Comprar blusa", pending: true },
+      ],
+    };
+  },
+  methods: {
+    addTask(task) {
+      //verifica se  nome jah esta na lista
+      const sameName = (t) => t.name === task.name;
+      //faz um filtro  de todas as tasks pra ver se tem um mesmo nome
+      const reallyNew = this.tasks.filter(sameName).length == 0;
+      // testar pra ver se realmente um novo nome
+      if (reallyNew) {
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true, //vai retornar true por padrao
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
