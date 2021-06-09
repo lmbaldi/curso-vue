b359539dd36adffb3eb8199f753e7cb7332d3d9f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-09 17:20:39 -0300

    manipulating comobox with <select> and <option>

--- formulario/formulario-exercicios/src/App.vue
+++ formulario/formulario-exercicios/src/App.vue
@@ -1,163 +1,193 @@
 <template>
-	<div id="app">
-		<h1>Registrar Reclamação</h1>
-		<div class="conteudo">
-			<form class="painel">
-				<div class="cabecalho">Formulário</div>
-				<Rotulo nome="E-mail">
-					<!--lazy envia a informacao depois que sai do campo -->
-					<input type="text" v-model.lazy.trim="usuario.email">
-				</Rotulo>
-				<Rotulo nome="Senha">
-					<input type="password" v-model="usuario.senha">
-				</Rotulo>
-				<Rotulo nome="Idade">
-					<input type="number" v-model.number="usuario.idade">
-				</Rotulo>
-				<Rotulo nome="Mensagem">
-					<textarea name="" cols="30" rows="5" v-model="mensagem"></textarea>
-				</Rotulo>
-				<Rotulo nome="Características do Problema">
-					<span class="mr-4">
-						<input type="checkbox" value="reproduzivel" v-model="caracteristicas"> Reproduzível
-					</span>
-					<span>
-						<input type="checkbox" value="intermitente" v-model="caracteristicas"> Intermitente
-					</span>
-				</Rotulo>
-				<Rotulo nome="Qual produto?">
-					<span class="mr-4"><input type="radio" value="web" v-model="produto"> Web</span>
-					<span class="mr-4"><input type="radio" value="mobile" v-model="produto"> Mobile</span>
-					<span><input type="radio" value="outro" v-model="produto"> Outro</span>
-				</Rotulo>
-				<Rotulo nome="Prioridade">
-					<select name="" id="">
-						<option></option>
-					</select>
-				</Rotulo>
-				<Rotulo nome="Primeira Reclamação?">
-					<Escolha />
-				</Rotulo>
-				<hr>
-				<button>Enviar</button>
-			</form>
-			<div class="painel">
-				<div class="cabecalho">Resultado</div>
-				<Rotulo nome="E-mail">
-					<span>{{ usuario.email }}</span>
-				</Rotulo>
-				<Rotulo nome="Senha">
-					<span>{{ usuario.senha }}</span>
-				</Rotulo>
-				<Rotulo nome="Idade">
-					<span>{{ usuario.idade }} {{ tipoIdade }}</span>
-				</Rotulo>
-				<Rotulo nome="Mensagem">
-					<span style="white-space: pre;">{{ mensagem }}</span>
-				</Rotulo>
-				<Rotulo nome="Características do problema">
-					<span>
-						<ul>
-							<li v-for="c in caracteristicas" :key="c"> {{ c }}</li>
-						</ul>
-					</span>
-				</Rotulo>
-				<Rotulo nome="Qual produto?">
-					<span>{{ produto }}</span>
-				</Rotulo>
-				<Rotulo nome="Prioridade">
-					<span>???</span>
-				</Rotulo>
-				<Rotulo nome="Primeira Reclamação?">
-					<span>???</span>
-				</Rotulo>
-			</div>
-		</div>
-	</div>
+  <div id="app">
+    <h1>Registrar Reclamação</h1>
+    <div class="conteudo">
+      <form class="painel">
+        <div class="cabecalho">Formulário</div>
+        <Rotulo nome="E-mail">
+          <!--lazy envia a informacao depois que sai do campo -->
+          <input type="text" v-model.lazy.trim="usuario.email" />
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <input type="password" v-model="usuario.senha" />
+        </Rotulo>
+        <Rotulo nome="Idade">
+          <input type="number" v-model.number="usuario.idade" />
+        </Rotulo>
+        <Rotulo nome="Mensagem">
+          <textarea name="" cols="30" rows="5" v-model="mensagem"></textarea>
+        </Rotulo>
+        <Rotulo nome="Características do Problema">
+          <span class="mr-4">
+            <input
+              type="checkbox"
+              value="reproduzivel"
+              v-model="caracteristicas"
+            />
+            Reproduzível
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              value="intermitente"
+              v-model="caracteristicas"
+            />
+            Intermitente
+          </span>
+        </Rotulo>
+        <Rotulo nome="Qual produto?">
+          <span class="mr-4">
+            <input type="radio" value="web" v-model="produto" />
+            Web
+          </span>
+          <span class="mr-4">
+            <input type="radio" value="mobile" v-model="produto" />
+            Mobile
+          </span>
+          <span>
+            <input type="radio" value="outro" v-model="produto" />
+            Outro
+          </span>
+        </Rotulo>
+        <Rotulo nome="Prioridade">
+          <select v-model='prioridade'>
+            <option
+              v-for="prioridade in prioridades"
+              :key="prioridade.codigo"
+              :selected="prioridade.codigo === 1"
+            >
+              {{ prioridade.codigo }} - {{ prioridade.nome }}
+            </option>
+          </select>
+        </Rotulo>
+        <Rotulo nome="Primeira Reclamação?">
+          <Escolha />
+        </Rotulo>
+        <hr />
+        <button>Enviar</button>
+      </form>
+      <div class="painel">
+        <div class="cabecalho">Resultado</div>
+        <Rotulo nome="E-mail">
+          <span>{{ usuario.email }}</span>
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <span>{{ usuario.senha }}</span>
+        </Rotulo>
+        <Rotulo nome="Idade">
+          <span>{{ usuario.idade }} {{ tipoIdade }}</span>
+        </Rotulo>
+        <Rotulo nome="Mensagem">
+          <span style="white-space: pre">{{ mensagem }}</span>
+        </Rotulo>
+        <Rotulo nome="Características do problema">
+          <span>
+            <ul>
+              <li v-for="c in caracteristicas" :key="c">{{ c }}</li>
+            </ul>
+          </span>
+        </Rotulo>
+        <Rotulo nome="Qual produto?">
+          <span>{{ produto }}</span>
+        </Rotulo>
+        <Rotulo nome="Prioridade">
+          <span>{{ prioridade }}</span>
+        </Rotulo>
+        <Rotulo nome="Primeira Reclamação?">
+          <span>???</span>
+        </Rotulo>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Rotulo from './components/Rotulo.vue'
-import Escolha from './components/Escolha.vue'
+import Rotulo from "./components/Rotulo.vue";
+import Escolha from "./components/Escolha.vue";
 
 export default {
-	name: 'app',
-	components: { Rotulo, Escolha },
-	computed:{
-		tipoIdade(){
-			return typeof this.usuario.idade
-		}
-	},
-	data(){
-		return {
-			mensagem: '',
-			caracteristicas: [],
-			produto: 'web',
-			usuario: {
-				email: '',
-				senha: '',
-				idade: 20,
-			},
-		}
-	}
-}
+  name: "app",
+  components: { Rotulo, Escolha },
+  computed: {
+    tipoIdade() {
+      return typeof this.usuario.idade;
+    },
+  },
+  data() {
+    return {
+      mensagem: "",
+      caracteristicas: [],
+      produto: "web",
+	  prioridade: 1,
+      prioridades: [
+        { codigo: 1, nome: "Baixa" },
+        { codigo: 2, nome: "Moderada" },
+        { codigo: 3, nome: "Alta" },
+      ],
+      usuario: {
+        email: "",
+        senha: "",
+        idade: 20,
+      },
+    };
+  },
+};
 </script>
 
 <style>
-
 body {
-	background-color: #ECECEC;
+  background-color: #ececec;
 }
 
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .conteudo {
-	display: flex;
+  display: flex;
 }
 
 .painel {
-	flex: 1;
-	background: #FFF;
-	margin: 0px 10px;
-	padding: 20px;
-	border: 1px solid #AAA;
-	border-radius: 5px;
+  flex: 1;
+  background: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
 }
 
 .painel .cabecalho {
-	width: 100%;
-	background-color: #DDD;
-	padding: 10px 0px;
-	border-radius: 5px;
-	font-size: 1.4rem;
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
 }
 
 #app form button {
-	float: right;
-	margin: 10px 0px;
-	padding: 10px 20px;
-	font-size: 1.4rem;
-	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
+  float: right;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
 }
 
 #app h1 {
-	font-weight: 200;
-	margin: 20px;
-	padding: 0;
+  font-weight: 200;
+  margin: 20px;
+  padding: 0;
 }
 
 .mr-4 {
-	margin-right: 40px;
+  margin-right: 40px;
 }
 </style>
