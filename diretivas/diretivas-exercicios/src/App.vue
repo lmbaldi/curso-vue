79b7e13f0b4f149a4f5e1c80a7301a811106bcf2
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-10 11:28:58 -0300

    using multiple modifiers

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -9,15 +9,17 @@
     <!-- <p v-destaque>Usando diretiva personalizada</p> -->
     <p v-destaque="'red'">Usando diretiva personalizada</p>
 
-    <p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
-
     <p v-destaque:fundo="'lightblue'">Usando diretiva personalizada</p>
 
-    <p v-destaque:fundo.atrasar="'grey'">Usando diretiva personalizada</p>
+    <p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
 
-    <p v-destaque-local:fundo="'lightblue'">Usando diretiva personalizada</p>
+    <p v-destaque:fundo.atrasar="'DarkGrey'">Usando diretiva personalizada</p>
 
-    <p v-destaque-local:fundo.atrasar="'grey'">Usando diretiva personalizada</p>
+    <p v-destaque-local:fundo.atrasar.alternar="'DeepSkyBlue'">
+      Usando diretiva personalizada
+    </p>
+
+    <p v-destaque-local.atrasar.alternar="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
@@ -27,20 +29,32 @@ export default {
   directives: {
     "destaque-local": {
       bind(el, binding, vnode) {
-        // el.style.backgroundColor = 'lightgreen'
-
+        const aplicarCor = (cor) => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = cor;
+          } else {
+            el.style.color = cor;
+          }
+        };
         let atraso = 0;
         if (binding.modifiers["atrasar"]) atraso = 3000;
 
+        const cor1 = binding.value;
+        const cor2 = "purple";
+        let corAtual = cor1;
+
         setTimeout(() => {
-          if (binding.arg === "fundo") {
-            el.style.backgroundColor = binding.value;
+          if (binding.modifiers["alternar"]) {
+            setInterval(() => {
+              corAtual = corAtual === cor1 ? cor2 : cor1
+              aplicarCor(corAtual);
+            }, 1000);
           } else {
-            el.style.color = binding.value;
+            aplicarCor(binding.value);
           }
         }, atraso);
-      },
-    },
+      }
+    }
   },
   data() {
     return {
