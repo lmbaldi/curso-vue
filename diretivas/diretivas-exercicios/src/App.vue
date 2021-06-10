0be28602cc7cefc7f04550815c9c063dbcec5c4e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-10 09:13:54 -0300

    passing value to custom directive

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -1,28 +1,34 @@
 <template>
-	<div id="app">
-		<h1>Diretivas</h1>
-		<hr>
-		<p v-text="'Usando diretiva <strong>v-html</strong>'"></p>
-		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
-		<hr>
-		<p v-destaque>Usando diretiva personalizada</p>
-	</div>
+  <div id="app">
+    <h1>Diretivas</h1>
+    <hr />
+    <p v-text="'Usando diretiva <strong>v-html</strong>'"></p>
+    <p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+    <hr />
+    <!-- <p v-destaque>Usando diretiva personalizada</p> -->
+	<p v-destaque="'red'">Usando diretiva personalizada</p>
+	<p v-destaque="cor">Usando diretiva personalizada</p>
+  </div>
 </template>
 
 <script>
 export default {
-	
-}
+	data(){
+		return {
+			cor: 'blue'
+		}
+	}
+};
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
 }
 </style>
