1b78f30e4c5123d0563e09ccc483ea4bc408d205
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-10 14:52:42 -0300

    solution exercise directive

--- diretivas/diretivas-desafio/src/App.vue
+++ diretivas/diretivas-desafio/src/App.vue
@@ -1,35 +1,63 @@
 <template>
-	<div id="app">
-		<h1>Diretivas (Desafio)</h1>
-		<hr>
-		<!-- Exercício -->
-		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
-	</div>
+  <div id="app">
+    <h1>Diretivas (Desafio)</h1>
+    <hr />
+    <!-- Exercício -->
+    <!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+    <button v-quando:click="acao">Executar</button>
+    <p v-quando:mouseenter="mouseEnter" v-quando:mouseleave="mouseLeave">
+      Testet mouse event
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-	
-}
+  directives: {
+    quando: {
+      bind(el, binding) {
+        // el.onclick = function (e) {
+        //   binding.value();
+        // };
+        const tipo = binding.arg;
+        const fn = binding.value;
+        el.addEventListener(tipo, fn);
+      },
+    },
+  },
+  methods: {
+    acao() {
+      alert("Ação executada!");
+    },
+    mouseEnter() {
+      // eslint-disable-next-line no-console
+      console.log("Mouse enter");
+    },
+    mouseLeave() {
+      // eslint-disable-next-line no-console
+      console.log("Mouse leave");
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
 }
 
 button {
-	margin: 10px 0px;
-	padding: 10px 20px;
-	font-size: 1.4rem;
-	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
 }
 </style>
