171fd38a8d2dd6b4400ed2b66f23408ef7798a72
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-10 20:54:58 -0300

    creating slide transition with css transition property

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -2,10 +2,15 @@
   <div id="app" class="container-fluid">
     <h1>Animações</h1>
     <hr />
-    <b-button variant="prymary" class="mb-4" @click="exibir = !exibir"
-      >Mostrar Mensagem</b-button
-    >
+    <b-button variant="prymary" class="mb-4" @click="exibir = !exibir">
+		Mostrar Mensagem
+	</b-button>
+
     <transition name="fade">
+      <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+    </transition>
+
+    <transition name="slide">
       <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
     </transition>
   </div>
@@ -33,12 +38,33 @@ export default {
   font-size: 1.5rem;
 }
 
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 2s;
 }
+
+@keyframes slide-in {
+	from { transform: translateY(40px) }
+	to { transform: translateY(0) }
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0) }
+	to { transform: translateY(40px) }
+}
+
+.slide-enter-active {
+	animation: slide-in 2s ease;
+}
+
+.slide-leave-active {
+	animation: slide-out 2s ease;
+}
+
 
 </style>
