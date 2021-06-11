72b74c52378fe58b1de15de5c9f45a69c83234fb
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-11 11:04:07 -0300

    using transition group to animate lists

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -64,9 +64,11 @@
     </transition> -->
     <hr>
     <b-button @click="adicionarAluno" class="mb-4">Adicionar Aluno</b-button>
-    <b-list-group v-for="(aluno, i) in alunos" :key="aluno">
-      <b-list-group-item @click="removerAluno(i)">{{ aluno }}</b-list-group-item>
-    </b-list-group>
+    <transition-group name="slide" tag="div">
+      <b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+        <b-list-group-item @click="removerAluno(i)">{{ aluno }}</b-list-group-item>
+      </b-list-group>
+    </transition-group>
   </div>
 </template>
 
@@ -191,6 +193,8 @@ export default {
 }
 
 .slide-leave-active {
+  position: absolute;
+  width: 100%;
   animation: slide-out 2s ease;
   transition: opacity 2s;
 }
@@ -198,5 +202,9 @@ export default {
 .slide-enter,
 .slide-leave-to {
   opacity: 0;
+}
+
+.slide-move{
+  transition: transform 1s;
 }
 </style>
