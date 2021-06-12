da334fb151f449a22cf58ac90d1f01a378fdd45c
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-12 16:58:36 -0300

    vue-router configuration

---
+++ rotas/rotas-exercicios/src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Inicio from './components/Inicio.vue'
+import Usuario from './components/usuario/Usuario.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            component: Inicio,
+        },
+        {
+            path: '/usuario',
+            component: Usuario,
+        }
+    ]
+})
