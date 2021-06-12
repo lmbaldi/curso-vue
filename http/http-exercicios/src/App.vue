a4817bb80708f0e86e0c3f85390d1f1d7e08084b
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-12 11:08:12 -0300

    sending post

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -37,7 +37,11 @@ export default {
   },
   methods: {
 	  salvar(){
-		  console.log(this.usuario)
+		this.$http.post('usuarios.json', this.usuario)
+			.then(resp => {
+				this.usuario.nome = ""
+				this.usuario.email = ""
+			})
 	  }
   }
   // created(){
