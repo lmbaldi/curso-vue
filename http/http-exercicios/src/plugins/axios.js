f88be2dfd36192e3d28cf46285306cc714f074a9
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-12 13:01:59 -0300

    intercepting response

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -1,19 +1,28 @@
 import Vue from 'vue'
 import axios from 'axios'
-import  baseUrl from '../constant_url' 
+import baseUrl from '../constant_url'
 
 // axios.defaults.baseURL = baseUrl.BASE_URL
 
 Vue.use({
-    install(Vue){
+    install(Vue) {
         // Vue.prototype.$http = axios
         Vue.prototype.$http = axios.create({
             baseURL: baseUrl.BASE_URL
         })
 
-        Vue.prototype.$http.interceptors.request.use( config => {
+        Vue.prototype.$http.interceptors.request.use(config => {
             console.log(config.method)
             return config
-        })
+        }, error => Promisse.reject(error))
+
+        Vue.prototype.$http.interceptors.response.use(res => {
+            const array = []
+            // for (let chave in res.data) {
+            //     array.push({ id: chave, ...res.data[chave] })
+            // }
+            // res.data = array
+            return res
+        }, error => Promisse.reject(error))
     }
 })
