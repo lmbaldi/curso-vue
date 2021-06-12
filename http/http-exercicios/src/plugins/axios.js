efa203df37221e34000a1105a7e9a62fff0ffc66
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-12 12:02:19 -0300

    intercep requests

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import axios from 'axios'
 import  baseUrl from '../constant_url' 
 
-axios.defaults.baseURL = baseUrl.BASE_URL
+// axios.defaults.baseURL = baseUrl.BASE_URL
 
 Vue.use({
     install(Vue){
-        Vue.prototype.$http = axios
+        // Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: baseUrl.BASE_URL
+        })
+
+        Vue.prototype.$http.interceptors.request.use( config => {
+            console.log(config.method)
+            return config
+        })
     }
 })
