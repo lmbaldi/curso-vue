51527552358206f16732727d108e78e11e86bd49
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-12 21:13:12 -0300

    latet loading routes

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from './components/Inicio.vue'
-import Usuario from './components/usuario/Usuario.vue'
-import UsuarioLista from './components/usuario/UsuarioLista.vue'
-import UsuarioDetalhe from './components/usuario/UsuarioDetalhe.vue'
-import UsuarioEditar from './components/usuario/UsuarioEditar.vue'
 import Menu from './components/template/Menu.vue'
 import MenuAlt from './components/template/MenuAlt.vue'
+// import Usuario from './components/usuario/Usuario.vue'
+// import UsuarioLista from './components/usuario/UsuarioLista.vue'
+// import UsuarioDetalhe from './components/usuario/UsuarioDetalhe.vue'
+//import UsuarioEditar from './components/usuario/UsuarioEditar.vue'
 
 Vue.use(Router)
+
+const Usuario = () => import(/* webpackChunkName: "usuario"*/'./components/usuario/Usuario.vue')
+const UsuarioDetalhe = () => import(/* webpackChunkName: "usuario"*/'./components/usuario/UsuarioDetalhe.vue')
+const UsuarioEditar = () => import(/* webpackChunkName: "usuario"*/'./components/usuario/UsuarioEditar.vue')
+const UsuarioLista = () => import(/* webpackChunkName: "usuario"*/'./components/usuario/UsuarioLista.vue')
 
 const router = new Router({
     mode: 'history',
