d4700428eb2ec34e41e4ac527ed37828191fb898
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-15 18:31:03 -0300

    configuring vuex in the project

--- projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -1,7 +1,7 @@
 <template>
- <v-layout row wrap>``
-   <Stock v-for="stock in stocks" :key="stock.id"/>
- </v-layout>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+  </v-layout>
 </template>
 
 <script>
@@ -9,16 +9,11 @@ import Stock from "./Stock";
 
 export default {
   components: { Stock },
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: "BMW", price: 110},
-        { id: 2, name: "Google", price: 200},
-        { id: 3, name: "Apple", price: 280},
-        { id: 4, name: "Twiter", price: 83},
-      ],
-    };
-  },
+  computed: {
+    stocks(){
+      return this.$store.getters.stocks
+    }
+  }
 };
 </script>
 
