261d23d735627449cc383662a4475a0e25ed8525
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-16 14:48:13 -0300

    applying monetary value filter to balance

--- projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -2,9 +2,9 @@
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
-        <strong
-          >{{ stock.name }} <small>(Preço: {{ stock.price }})</small></strong
-        >
+        <strong>
+          {{ stock.name }} <small>(Preço: {{ stock.price | currency }})</small>
+        </strong>
       </v-card-title>
     </v-card>
     <v-card>
@@ -15,11 +15,14 @@
           :error="insufficientFunds || !Number.isInteger(quantity)"
           v-model.number="quantity"
         />
-        <v-btn class="green darken-3 white--text" 
-            @click="buyStock"
-            :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
-          >{{ insufficientFunds ? 'Insuficiente' : 'Comprar'}}</v-btn
-        >
+        <v-btn
+          class="green darken-3 white--text"
+          @click="buyStock"
+          :disabled="
+            insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)
+          "
+          >{{ insufficientFunds ? "Insuficiente" : "Comprar" }}
+        </v-btn>
       </v-container>
     </v-card>
   </v-flex>
@@ -34,12 +37,12 @@ export default {
     };
   },
   computed: {
-    funds(){
-      return this.$store.getters.funds
+    funds() {
+      return this.$store.getters.funds;
     },
-    insufficientFunds(){
-      return this.quantity * this.stock.price > this.funds
-    }
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
+    },
   },
   methods: {
     buyStock() {
@@ -48,8 +51,7 @@ export default {
         stockPrice: this.stock.price,
         quantity: this.quantity,
       };
-
-      this.$store.dispatch('buyStock', order)
+      this.$store.dispatch("buyStock", order);
       this.quantity = 0;
     },
   },
