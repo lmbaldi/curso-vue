4771b2b13422604d600b5df7b10435c608f33f61
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-18 14:21:43 -0300

    button component #2

--- vue/calculator/src/main/Calculator.vue
+++ vue/calculator/src/main/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="calculator">
-      <Display />
-      <Button label="AC" triple/>
-      <Button label="/" operation/>
-      <Button label="7"/>
-      <Button label="8"/>
-      <Button label="9"/>
-      <Button label="*" operation/>
-      <Button label="4"/>
-      <Button label="5"/>
-      <Button label="6"/>
-      <Button label="-" operation/>
-      <Button label="1"/>
-      <Button label="2"/>
-      <Button label="3"/>
-      <Button label="+" operation/>
-      <Button label="0" double/>
-      <Button label="."/>
-      <Button label="=" operation/>
+    <Display />
+    <Button label="AC" triple @onClick="clearMemory" />
+    <Button label="/" operation @onClick="setOperation" />
+    <Button label="7" @onClick="addDigit" />
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" operation @onClick="setOperation"/>
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" operation @onClick="setOperation"/>
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" operation @onClick="setOperation"/>
+    <Button label="0" double @onClick="addDigit" />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" operation @onClick="setOperation"/>
   </div>
 </template>
 
@@ -27,19 +27,24 @@ import Button from "@/components/Button.vue";
 
 export default {
   components: { Button, Display },
+  methods: {
+    clearMemory() {},
+    setOperation(operation) {},
+
+    addDigit(n) {},
+  },
 };
 </script>
 
 <style >
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-.calculator{
-    height: 320px;
-    width: 235px;
-    border-radius: 5px;
-    overflow: hidden;
-
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>0
