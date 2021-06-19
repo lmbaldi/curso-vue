2c719f6e2c0347bfce2c6c4aa507639c96fee93c
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-19 14:04:32 -0300

    Monty Hall problem #1

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,24 +1,26 @@
 <template>
-    <div id="app">
-        <h2> Problema de Monty Hall</h2>
-    </div>
+  <div id="app">
+    <h2>Problema de Monty Hall</h2>
+    <Gift />
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import Gift from "@/components/Gift";
+export default {
+  name: "App",
+  components: { Gift },
+};
 </script>
 
 <style >
-*  {
-    box-sizing: border-box;
-    font-family: 'Montserrat', sans-serif;
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
 }
 
 body {
-    color: #fff;
-    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153))
+  color: #fff;
+  background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
 }
-
 </style>
