6c87ae6c432c9dc0826476143f039e1bfce78f0f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-19 13:13:58 -0300

    add Monty Hall project

---
+++ vue/montyhall/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <h2> Problema de Monty Hall</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style >
+*  {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+
+body {
+    color: #fff;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153))
+}
+
+</style>
