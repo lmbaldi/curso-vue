f05ea0ab2974be209bac856d27117a5786e077fb
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-19 14:54:36 -0300

    Monty Hall problem #2

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
     <h2>Problema de Monty Hall</h2>
-    <Gift />
+    <Door number="1" :hasGift="false" />
   </div>
 </template>
 
 <script>
-import Gift from "@/components/Gift";
+import Door from "@/components/Door";
 export default {
   name: "App",
-  components: { Gift },
+  components: { Door },
 };
 </script>
 
@@ -22,5 +22,17 @@ export default {
 body {
   color: #fff;
   background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h2 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
 }
 </style>
